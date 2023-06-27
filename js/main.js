5ce3596a019dc4d54e47c6a9e363f831f153ae6b
Ravi kumar <1002kumarravi@gmail.com>
2023-06-27 22:11:51 +0530

    validation for start simulations

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ const simulationsWrapper = document.querySelector(".simulations-wrapper");
 const liftCount = document.querySelector(".lift-count");
 const floorCount = document.querySelector(".floor-count");
 
+const show = document.querySelector(".show");
+
 const homeData = {
   liftCount: 0,
   floorCount: 0,
 };
 
-liftCount.addEventListener("change", (e) => {
+liftCount.addEventListener("keyup", (e) => {
   homeData.liftCount = e.target.value;
 });
 
@@ -20,7 +22,17 @@ floorCount.addEventListener("change", (e) => {
   homeData.floorCount = e.target.value;
 });
 
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(homeData);
+  if (homeData.floorCount <= 0 ||  homeData.floorCount > 50) {
+    alert("please fill valid floor");
+    return;
+  } else if (homeData.liftCount <= 0 || homeData.liftCount > 10) {
+    alert("please fill valid lift");
+    return;
+  }
+  console.log("A");
   homeWrapper.classList.add("hide");
   simulationsWrapper.classList.remove("hide");
 });
@@ -28,4 +40,8 @@ startBtn.addEventListener("click", () => {
 backBtn.addEventListener("click", () => {
   simulationsWrapper.classList.add("hide");
   homeWrapper.classList.remove("hide");
+});
+
+show.addEventListener("click", () => {
+  console.log(homeData);
 });
