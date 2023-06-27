525401227e04f8362ca2afd03a6f17a7906b0e73
Ravi kumar <1002kumarravi@gmail.com>
2023-06-27 18:54:19 +0530

    add lift simulation route

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
+const startBtn = document.querySelector(".start-btn");
+const backBtn = document.querySelector(".back-btn");
 
-const startBtn = document.querySelector(".start-btn")
+const homeWrapper = document.querySelector(".home-wrapper");
+const simulationsWrapper = document.querySelector(".simulations-wrapper");
 
-// startBtn.addEventListener("click", )
+const liftCount = document.querySelector(".lift-count");
+const floorCount = document.querySelector(".floor-count");
+
+liftCount.addEventListener("change", (e) => {
+  console.log(e.target.value, "liftCount");
+});
+
+floorCount.addEventListener("change", (e) => {
+  console.log(e.target.value, "floorCount");
+});
+
+startBtn.addEventListener("click", () => {
+  homeWrapper.classList.add("hide");
+  simulationsWrapper.classList.remove("hide");
+});
+
+backBtn.addEventListener("click", () => {
+  simulationsWrapper.classList.add("hide");
+  homeWrapper.classList.remove("hide");
+});
