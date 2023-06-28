1d6dda65a7535227d17fd839c6868cd941682861
Ravi kumar <1002kumarravi@gmail.com>
2023-06-28 10:23:37 +0530

    input lift layout

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 const startBtn = document.querySelector(".start-btn");
 const backBtn = document.querySelector(".back-btn");
-
 const homeWrapper = document.querySelector(".home-wrapper");
 const simulationsWrapper = document.querySelector(".simulations-wrapper");
-
 const liftCount = document.querySelector(".lift-count");
 const floorCount = document.querySelector(".floor-count");
-
 const show = document.querySelector(".show");
 
 const homeData = {
@@ -22,13 +19,38 @@ floorCount.addEventListener("change", (e) => {
   homeData.floorCount = e.target.value;
 });
 
-const createLayoutOfFloor = (lift, floor) => {
+const createElementAndAddAttr = (elementTag, className, id) => {
+  const element = document.createElement(elementTag);
+  if (className) element.setAttribute("class", className);
+  if (id) element.setAttribute("id", id);
+  return element;
+};
+
+const createLiftforFloor = (totalLift, liftDetails) => {
+  console.log(totalLift, liftDetails);
+  for (let i = 0; i <= totalLift; i++) {
+    const liftLayout = createElementAndAddAttr("div", "lift-layout");
+    liftLayout.innerHTML = `<div></div><div></div>`;
+    liftDetails.append(liftLayout);
+  }
+};
+
+const createLayoutOfFloor = (floor, lift) => {
   simulationsWrapper.innerHTML = "";
-  const floorContainer = document.createElement("div");
-  const liftContainer = document.createElement("div");
-  const liftBtnContainer = document.createElement("div");
-  const liftUpBtn = document.createElement("button");
-  const liftDownBtn = document.createElement("button");
+  const floorLiftContainer = createElementAndAddAttr(
+    "div",
+    "floor-lift-container"
+  );
+  const floorDetails = createElementAndAddAttr("div", "floor-details");
+  const liftBtnContainer = createElementAndAddAttr("div", "lift-btn-container");
+  const liftDetails = createElementAndAddAttr("div", "lift-details");
+
+  for (let i = 0; i <= floor; i++) {
+    floorDetails.innerHTML = `<h2>Floor 1</h2> <div class="lift-btn-container"> <button class="lift-btn"> ⬆ </button> <button class="lift-btn"> ⬇</button></div>`;
+    simulationsWrapper.append(floorLiftContainer);
+    floorLiftContainer.append(floorDetails, liftDetails);
+  }
+  createLiftforFloor(lift, liftDetails);
 };
 
 startBtn.addEventListener("click", (e) => {
@@ -44,6 +66,7 @@ startBtn.addEventListener("click", (e) => {
   console.log("A");
   homeWrapper.classList.add("hide");
   simulationsWrapper.classList.remove("hide");
+  createLayoutOfFloor(homeData.floorCount, homeData.liftCount);
 });
 
 backBtn.addEventListener("click", () => {
